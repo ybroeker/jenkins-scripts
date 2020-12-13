3eece214a062159016c30bf300a1449fd3d12816
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-12-13 18:01:15 +0100

    Create file-size-stats.groovy

---
+++ file-size-stats.groovy
@@ -0,0 +1,33 @@
+import java.time.*;
+import hudson.plugins.sloccount.*;
+  
+def jobPattern = "gse-2020-[a-zA-Z0-9]+-ms1\$"
+
+
+println "Matching all Jobs to: '${jobPattern}'"
+def matchedJobs = Jenkins.instance.items.findAll { job ->
+    job.name =~ /$jobPattern/
+}
+
+projectsStats = matchedJobs.stream().map({it.lastSuccessfulBuild})
+    .filter({it!=null})
+    .map({it.getAction(SloccountBuildAction.class).result.report})
+    .mapToInt({it.lineCount})
+    .summaryStatistics()
+
+fileStats = matchedJobs.stream().map({it.lastSuccessfulBuild})
+    .filter({it!=null})
+    .map({it.getAction(SloccountBuildAction.class).result.report})
+    .flatMap({it.files.stream()})
+    .mapToInt({it.lineCount})
+    .summaryStatistics()
+
+nrOfFiles = matchedJobs.stream().map({it.lastSuccessfulBuild})
+    .filter({it!=null})
+    .map({it.getAction(SloccountBuildAction.class).result.report})
+    .mapToInt({it.files.size})
+    .summaryStatistics()
+
+println("projectsStats: ${projectsStats}")
+println("fileStats: ${fileStats}")
+println("nrOfFiles: ${nrOfFiles}")
